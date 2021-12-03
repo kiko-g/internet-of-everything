bdef792fbb2777871508a8c65ed9e340f7ee363e
nunomiguel22 <nunomiguel22@LENOVO-LAPTOP.localdomain>
2021-12-03 17:07:47 +0000

    skeleton update

--- chaos_engineering/delete_data_reading.py
+++ chaos_engineering/delete_data_reading.py
@@ -1,10 +1,15 @@
 import requests
 
-request = requests.get("http://localhost:8000/machine1",
+""" request = requests.get("http://localhost:8000/machine1",
                 headers= {
                     "User-Agent":
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
                 }
-            )
+            ) """
 
-print(request.json())
+
+
+
+request = requests.post('http://localhost:8000/fault', json={"key": "value"})
+
+#print(request.json())
--- chaos_engineering/launch_mock_server.py
+++ chaos_engineering/launch_mock_server.py
@@ -10,8 +10,22 @@ def get_machine1():
 def get_machine2():
     return get_json_from_file("static/machine2.json")
 
+def central_handler():
+    print("central handler")
+
+def central_response():
+    return "received json data"
+
+def fault_handler():
+    print("central handler")
+
+def fault_response():
+    return "received json data"
 
 server.add_endpoint(endpoint='/machine1', endpoint_name='machine1', response=get_machine1)
 server.add_endpoint(endpoint='/machine2', endpoint_name='machine2', response=get_machine2)
+server.add_endpoint(endpoint='/central', endpoint_name='central', handler=central_handler, response=central_response)
+server.add_endpoint(endpoint='/fault', endpoint_name='fault', handler=fault_handler, response=fault_response)
+
 
 server.run()
