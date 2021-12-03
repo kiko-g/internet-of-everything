bc3af920366961a5b24cc6ca0b0abff13db4ba8c
nunomiguel22 <nunomiguel22@LENOVO-LAPTOP.localdomain>
2021-12-03 16:46:30 +0000

    skeleton update

--- chaos_engineering/launch_mock_server.py
+++ chaos_engineering/launch_mock_server.py
@@ -1,12 +1,17 @@
 from mock_server import MockServer
-import json
-
-def get_machine1():
-    with open("static/machine1.json", 'r') as jsonFile:
-        jsonObject = json.load(jsonFile)
-        jsonFile.close()
-        return json.dumps(jsonObject)
+from json_helper import get_json_from_file
 
 server = MockServer(8000)
+
+
+def get_machine1():
+    return get_json_from_file("static/machine1.json")
+
+def get_machine2():
+    return get_json_from_file("static/machine2.json")
+
+
 server.add_endpoint(endpoint='/machine1', endpoint_name='machine1', response=get_machine1)
+server.add_endpoint(endpoint='/machine2', endpoint_name='machine2', response=get_machine2)
+
 server.run()
--- chaos_engineering/mock_server/mock_server.py
+++ chaos_engineering/mock_server/mock_server.py
@@ -5,12 +5,13 @@ class EndpointAction(object):
     def __init__(self, action, response):
         self.action = action
         if response:
-          response_content = response()
-        self.response = Response(status=200, headers={}, mimetype='application/json', response=response_content)
+            response_content = response()
+        self.response = Response(status=200,
+            headers={}, mimetype='application/json', response=response_content)
 
     def __call__(self, *args):
         if self.action:
-          self.action()
+            self.action()
         return self.response
 
 
@@ -26,3 +27,4 @@ class MockServer(object):
 
     def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None, response=None):
         self.app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler, response))
+
