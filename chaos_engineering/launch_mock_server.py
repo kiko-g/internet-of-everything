49c5ef7def2b4ac30a01c846ea870b8347458e42
nunomiguel22 <nunomiguel22@LENOVO-LAPTOP.localdomain>
2021-12-03 17:35:55 +0000

    skeleton update

--- chaos_engineering/launch_mock_server.py
+++ chaos_engineering/launch_mock_server.py
@@ -1,9 +1,6 @@
 from mock_server import MockServer
 from json_helper import get_json_from_file
 
-server = MockServer(8000)
-
-
 def get_machine1():
     return get_json_from_file("static/machine1.json")
 
@@ -23,6 +20,8 @@ def fault_response():
     return "received json data"
 
 def main():
+    server = MockServer(8000)
+
     server.add_endpoint(endpoint='/machine1', endpoint_name='machine1', response=get_machine1)
     server.add_endpoint(endpoint='/machine2', endpoint_name='machine2', response=get_machine2)
     server.add_endpoint(endpoint='/central', endpoint_name='central', handler=central_handler, response=central_response)
