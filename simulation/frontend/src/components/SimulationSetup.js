2bdc8bd0bc14c95aaf7d02b645c8e5e404c92b74
kiko-g <kikojpgoncalves@gmail.com>
2021-12-04 19:18:59 +0000

    switched branches and added run button

--- simulation/frontend/src/components/SimulationSetup.js
+++ simulation/frontend/src/components/SimulationSetup.js
@@ -1,9 +1,10 @@
 import * as React from "react";
 import Basic from './input/Basic'
+import Button from './partials/Button'
 
 export default function SimulationSetup() {
   return (
-    <div className="hidden md:grid grid-cols-4 space-x-0 dark:text-gray-50">
+    <div className="hidden md:grid md:grid-cols-4 space-x-0 dark:text-gray-50">
       <div className="col-span-3 p-8 pt-4 dark:bg-bluegray-600">
         <div className="grid grid-cols-3 gap-y-4 gap-x-16">
           <Basic label="Amount" />
@@ -14,7 +15,7 @@ export default function SimulationSetup() {
         </div>
       </div>
       <div className="col-span-1 p-3 dark:bg-bluegray-600">
-        Run section
+        <Button text="Run" />
       </div>
     </div>
   );
