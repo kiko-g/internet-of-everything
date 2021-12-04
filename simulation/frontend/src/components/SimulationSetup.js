99b63a0696c54df2a379a8ace76be4ab09607001
joao <joaopaulo_n@hotmail.com>
2021-12-04 23:10:03 +0000

    Added Start and Save JSON buttons #26

--- simulation/frontend/src/components/SimulationSetup.js
+++ simulation/frontend/src/components/SimulationSetup.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Basic from './input/Basic'
-import Button from './partials/Button'
 
 export default function SimulationSetup() {
   return (
@@ -14,8 +13,21 @@ export default function SimulationSetup() {
           <Basic label="Another one" />
         </div>
       </div>
-      <div className="col-span-1 p-3 dark:bg-bluegray-600">
-        <Button text="Run" />
+
+      <div class="p-4 flex">
+        <p class="text-xl mt-2 ml-16 dark:bg-bluegray-600">Start&nbsp;&nbsp;</p>
+      
+        <div className="dark:bg-bluegray-600">
+          <button className={`p-2 rounded-6xl border-2 border-teal-800 text-coolgray-200 bg-teal-400 hover:opacity-50 duration-100`}>{
+           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 23"><path d="M3 22v-20l18 10-18 10z"/></svg>}</button>  
+        </div>
+
+        <p class="text-xl mt-2 dark:bg-bluegray-600">&nbsp;&nbsp;Save JSON&nbsp;&nbsp;</p>
+      
+        <div className="dark:bg-bluegray-600">
+          <button className={`p-2 rounded-6xl border-2 border-teal-800 text-coolgray-200 bg-blue-400 hover:opacity-50 duration-100`}>{
+           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 11h5l-9 10-9-10h5v-11h8v11zm1 11h-10v2h10v-2z"/></svg>}</button>  
+        </div>
       </div>
     </div>
   );
