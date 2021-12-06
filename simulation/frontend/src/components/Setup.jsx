81a4454ad9f5803011ae38c216ce63217386acca
kiko-g <kikojpgoncalves@gmail.com>
2021-12-06 23:34:56 +0000

    Major refactor to UI of the simulation setup

--- simulation/frontend/src/components/Setup.jsx
+++ simulation/frontend/src/components/Setup.jsx
@@ -1,97 +1,51 @@
 import * as React from "react"
-import { Disclosure } from "@headlessui/react"
-import { MinusSmIcon, PlusSmIcon } from "@heroicons/react/solid"
 import RunSection from "./RunSection"
-
-const filters = [
-  {
-    id: "color",
-    name: "Color",
-    options: [
-      { value: "white", label: "White", checked: false },
-      { value: "beige", label: "Beige", checked: false },
-      { value: "blue", label: "Blue", checked: true },
-      { value: "brown", label: "Brown", checked: false },
-      { value: "green", label: "Green", checked: false },
-      { value: "purple", label: "Purple", checked: false },
-    ],
-  },
-  {
-    id: "category",
-    name: "Category",
-    options: [
-      { value: "new-arrivals", label: "New Arrivals", checked: false },
-      { value: "sale", label: "Sale", checked: false },
-      { value: "travel", label: "Travel", checked: true },
-      { value: "organization", label: "Organization", checked: false },
-      { value: "accessories", label: "Accessories", checked: false },
-    ],
-  },
-  {
-    id: "size",
-    name: "Size",
-    options: [
-      { value: "2l", label: "2L", checked: false },
-      { value: "6l", label: "6L", checked: false },
-      { value: "12l", label: "12L", checked: false },
-      { value: "18l", label: "18L", checked: false },
-      { value: "20l", label: "20L", checked: false },
-      { value: "40l", label: "40L", checked: true },
-    ],
-  },
-]
+import Collapsed from "./utilities/Collapsed"
+import PresetsRadio from "./utilities/PresetsRadio"
+import { PlusCircleIcon } from "@heroicons/react/solid"
 
 export default function Setup() {
   return (
     <div>
-      <form className="grid grid-cols-2 gap-x-4 gap-y-1">
+      <form className="grid grid-cols-2 gap-4">
         <div className="col-span-2">
-          <RunSection />
+          <RunSection content="aaaa" />
         </div>
-        {filters.map((section) => (
-          <Disclosure as="div" key={section.id} className="py-5">
-            {({ open }) => (
-              <>
-                <h3 className="-my-3 flow-root">
-                  <Disclosure.Button className="p-3 rounded-md bg-coolgray-100 w-full flex items-center justify-between text-sm text-gray-400 hover:text-gray-500">
-                    <span className="font-medium text-gray-900">
-                      {section.name}
-                    </span>
-                    <span className="ml-6 flex items-center">
-                      {open ? (
-                        <MinusSmIcon className="h-5 w-5" aria-hidden="true" />
-                      ) : (
-                        <PlusSmIcon className="h-5 w-5" aria-hidden="true" />
-                      )}
-                    </span>
-                  </Disclosure.Button>
-                </h3>
-                <Disclosure.Panel className="pt-6">
-                  <div className="space-y-2">
-                    {section.options.map((option, optionIdx) => (
-                      <div key={option.value} className="flex items-center">
-                        <input
-                          id={`filter-${section.id}-${optionIdx}`}
-                          name={`${section.id}[]`}
-                          defaultValue={option.value}
-                          type="checkbox"
-                          defaultChecked={option.checked}
-                          className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-                        />
-                        <label
-                          htmlFor={`filter-${section.id}-${optionIdx}`}
-                          className="ml-3 text-sm text-gray-600"
-                        >
-                          {option.label}
-                        </label>
-                      </div>
-                    ))}
-                  </div>
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
-        ))}
+
+        <div className="col-span-2">
+          <div className="bg-bluegray-100 p-4 rounded-lg">
+            <PresetsRadio />
+            <div className="mt-2 flex items-end justify-end">
+              <button
+                type="button"
+                onClick=""
+                className="px-3 py-2 text-sm font-medium text-white bg-bluegray-700 rounded-md bg-opacity-30 hover:bg-opacity-40 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+              >
+                Add preset&nbsp;
+                <PlusCircleIcon className="w-4 h-4 mb-0.5 inline-flex" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-2">
+          <div className="bg-bluegray-100 p-4 rounded-lg w-full space-y-4 mx-auto">
+            <Collapsed color="bluegray" headline="Setting A">
+              Settings
+            </Collapsed>
+            <Collapsed color="bluegray" headline="Setting B">
+              Settings
+            </Collapsed>
+            <div className="grid grid-cols-2 space-x-2">
+              <Collapsed color="bluegray" headline="Setting C">
+                Settings
+              </Collapsed>
+              <Collapsed color="bluegray" headline="Setting D">
+                Settings
+              </Collapsed>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   )
