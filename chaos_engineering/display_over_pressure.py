86849913d2034efa770ffd37a427cb4622c2dec7
Joao Vasconcelos <up201504397@fe.up.pt>
2021-12-17 16:59:03 +0000

    feat:display over-pressure

---
+++ chaos_engineering/display_over_pressure.py
@@ -0,0 +1,24 @@
+import requests
+import random
+import json
+
+def main():
+    request = request.get("http://localhost:8000/machine2",
+                headers= {
+                    "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
+                }
+                            )
+    machine_json = request.json()
+    machine_properties = machine_json['properties']
+    pressure = random.uniform(0,100000)
+    machine_properties['pressure'] = pressure
+    
+    last_json = json.dumps(machine_json)
+    requests.post('http://localhost:8000/fault', last_json)
+    
+    
+    if __name__ == '__main__':
+        main()
+        
+        
