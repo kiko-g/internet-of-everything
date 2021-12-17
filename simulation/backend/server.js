709081c28d79d89ef945feafe6db391cb8263936
Luijinho <67716453+Luijinho@users.noreply.github.com>
2021-12-17 17:01:17 +0000

    feat: Receive body from frontend

--- simulation/backend/server.js
+++ simulation/backend/server.js
@@ -7,15 +7,30 @@ const app = express();
 const PORT = process.env.port || 8080;
 
 app.use(json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5000");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 app.use(urlencoded({ extended: true }));
 
-app.get("/startSimulation", (req, res) => {
-    //TODO: Receive user input from front end
-    let simulation = new Simulation("./factory.json", 30);
-    
-    let endState = simulation.run();
+app.post("/startSimulation", (req, res) => {
+    //let machineID = req.body.machineID;
+    let piecesQty = req.body.piecesQty;
 
-    res.send(endState);
+    if (isNaN(piecesQty))
+        res.status(400).send("A quantidade de peças tem de ser um número.");
+    /* else if (isNaN(machineID))
+        res.status(400).send("A ID da máquina tem de ser um número."); */ else {
+        //TODO: Receive user input from front end
+        let simulation = new Simulation("./factory.json", piecesQty);
+
+        let endState = simulation.run();
+
+        res.send(endState);
+    }
 });
 
-app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Server listening at http://localhost:${PORT}`)
+);
