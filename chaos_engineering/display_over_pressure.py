d9d0f69797ecdd6191273c160fcd27c890b49252
Joao Vasconcelos <up201504397@fe.up.pt>
2021-12-22 02:15:03 +0000

    refactor:spaces

--- chaos_engineering/display_over_pressure.py
+++ chaos_engineering/display_over_pressure.py
@@ -1,13 +1,18 @@
+"""
+This module POSTs a machine json with a fake pressure value
+"""
+
 import random
 import json
 import requests
 
 def main():
-    request = request.get("http://localhost:8000/machine2",
-                headers={
-                    "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
-                    })
+    browser_type = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
+    request = requests.get("http://localhost:8000/machine2",
+                            headers={
+                                "User-Agent":
+                                browser_type
+                                })
     
     machine_json = request.json()
     machine_properties = machine_json['properties']
