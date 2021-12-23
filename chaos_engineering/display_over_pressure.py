a61bf8f342b8493199acb4dd16ff60cd438cc0ba
Joao Vasconcelos <up201504397@fe.up.pt>
2021-12-23 02:34:51 +0000

    refactor : added docstring

--- chaos_engineering/display_over_pressure.py
+++ chaos_engineering/display_over_pressure.py
@@ -8,6 +8,7 @@ import requests
 
 
 def main():
+    """ Launch display over-pressure script """
     browser_type = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
     request = requests.get("http://localhost:8000/machine2",
                            headers={
