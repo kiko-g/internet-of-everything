62fe1b4ab99a23d1a6c52ba086b6fae6397f1ddb
Luijinho <67716453+Luijinho@users.noreply.github.com>
2022-01-07 13:59:37 +0000

    feat: communication between backend and frontend

--- simulation/backend/server.js
+++ simulation/backend/server.js
@@ -1,36 +1,40 @@
-import express from "express";
-import pkg from "body-parser";
-const { json, urlencoded } = pkg;
-import Simulation from "./Simulation.js";
+import express from "express"
+import pkg from "body-parser"
+const { json, urlencoded } = pkg
+import Simulation from "./Simulation.js"
 
-const app = express();
-const PORT = process.env.port || 8080;
+const app = express()
+const PORT = process.env.port || 8080
 
-app.use(json());
+app.use(json())
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-app.use(urlencoded({ extended: true }));
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+})
+app.use(urlencoded({ extended: true }))
 
 app.post("/startSimulation", (req, res) => {
-  //let machineID = req.body.machineID;
-  let piecesQty = req.body.piecesQty;
+  // If JSON from user input
+  let userJson = req.body.userJson
+  if (
+    userJson.batches == undefined ||
+    userJson.startMachineID == undefined ||
+    userJson.machines == undefined
+  ) {
+    res.status(400).send("Missing attributes on JSON.")
+  } else {
+    let simulation = new Simulation(userJson)
 
-  if (isNaN(piecesQty))
-    res.status(400).send("A quantidade de peças tem de ser um número.");
-  /* else if (isNaN(machineID))
-        res.status(400).send("A ID da máquina tem de ser um número."); */ else {
-    //TODO: Receive user input from front end
-    let simulation = new Simulation("factory.json", piecesQty);
+    // If JSON input from file
+    //let simulation = new Simulation("factory.json")
 
-    let endState = simulation.run();
+    let endState = simulation.run()
 
-    res.send(endState);
+    res.status(200).send(endState)
   }
-});
+})
 
 app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
-);
+)
