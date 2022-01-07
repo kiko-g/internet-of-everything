f3f78a67fa84beaeec61bdf38cb0dad9de93384b
JoaoCarlosPires <joaocarlosmrp@gmail.com>
2022-01-07 12:08:48 +0000

    pylint tests: passed

--- chaos_engineering/sequential_errors.py
+++ chaos_engineering/sequential_errors.py
@@ -8,39 +8,40 @@ import os
 
 def main():
     """ Launch inject sequential errors script"""
-    
-    machineID = -1
+    machine_id = -1
     repetition = -1
 
-    # Control cycle to check if the machine ID is not negative and different from int
-    while machineID < 1:
+    # Control cycle to check if the machine ID is not negative and different
+    # from int
+    while machine_id < 1:
         while True:
             try:
-                machineID = int(input("Please insert the machineID: "))
+                machine_id = int(input("Please insert the machine_id: "))
                 break
             except ValueError:
-                print("Please input integer only...")  
+                print("Please input integer only...")
                 continue
-
-    # Control cycle to check if the repetition number is not negative and different from int
+    # Control cycle to check if the repetition number is not negative and
+    # different from int
     while repetition < 1:
         while True:
             try:
-                repetition = int(input("Please insert the number of repetitions: "))
+                repetition = int(
+                    input("Please insert the number of repetitions: "))
                 break
             except ValueError:
-                print("Please input integer only...")  
+                print("Please input integer only...")
                 continue
 
-    for i in range (0, repetition):
+    for _ in range(0, repetition):
         rand = randrange(3)
         if rand == 0:
-            os.system("python3 display_overheating.py " + machineID)
+            os.system("python3 display_overheating.py " + machine_id)
         elif rand == 1:
-            os.system("python3 display_over_pressure.py " + machineID)
+            os.system("python3 display_over_pressure.py " + machine_id)
         else:
-            os.system("python3 display_overvibration.py " + machineID)
-        
+            os.system("python3 display_overvibration.py " + machine_id)
+
 
 if __name__ == '__main__':
     main()
