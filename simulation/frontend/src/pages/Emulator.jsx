bd868deb97e53f81a51e048dd7d7af52c034cb5a
kiko-g <kikojpgoncalves@gmail.com>
2022-01-22 17:22:29 +0000

    feat: header references current page

--- simulation/frontend/src/pages/Emulator.jsx
+++ simulation/frontend/src/pages/Emulator.jsx
@@ -5,7 +5,7 @@ import EmulatorRepresentation from "../components/EmulatorRepresentation"
 export default function Emulator() {
   return (
     <div className="homepage min-h-screen bg-slate-200 dark:bg-slate-500 font-sans font-medium">
-      <Header siteTitle="Emulator" />
+      <Header location="Emulator" />
       <div className="min-h-adjusted mx-auto p-6">
         <div className="grid grid-cols-12 space-x-0 space-y-8 xl:space-x-8 xl:space-y-0">
           <div className="col-span-12 xl:col-span-12 m-0">
